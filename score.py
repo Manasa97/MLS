aac446994feb6342d2aa1addb2ba75abc552d7dd
Manasa97 <manasa.nallabotula@gmail.com>
2017-11-26 15:28:41 +0530

    Update score.py

--- score.py
+++ score.py
@@ -1,18 +1,35 @@
-
-letterScores = {}
-
-def initiateLetterScores():
+#dir is 0 for right and 1 for down
+#(0,0) is the top left corner of the board
+def initLetterScores():
     letterScores = {"-":0,"aeioulnrst":1,"dg":2,"bcmp":3,"fhvwy":4,"k":1,"jx":8,"qz":10}
     return letterScores
 
+def initBoard():
+    board = []
+    initDict = {"letter":"0","letterBonus":1,"wordBonus":1}
+    list = []
+    for j in range(15):
+        list.append(initDict)
+    for i in range(15):
+        board.append(list)
+    return board
+    #*15 doesn't work
 
-def computeScore(word,letterScores):
+def computeScore(word, row, col, dir,board,letterScores):#board, row no, col no, dir
     sum = 0
-    print letterScores
+    bonusScore = 1
     for i in word:
         for j in letterScores.keys():
            if i in j:
-                sum = sum + letterScores[j]
+                sum = sum + letterScores[j] * board[row][col]["letterBonus"]
+                bonusScore = bonusScore * board[row][col]["wordBonus"]
+        row = row + dir
+        col = col + (1 - dir)
+        
+    sum = sum * bonusScore
     return sum
 
-print(computeScore("idiot",initiateLetterScores()))
+
+board = initBoard()
+letterScores = initLetterScores()
+print(computeScore("mentally",1,6,0,board,letterScores))
