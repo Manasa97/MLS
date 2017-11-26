1e9e1a0b53a917f5bde4d1b697d640f04dca0680
Manasa97 <manasa.nallabotula@gmail.com>
2017-11-26 16:52:42 +0530

    Add files via upload
    Bonus scores initialised, score computation complete

--- score.py
+++ score.py
@@ -1,19 +1,55 @@
+from copy import deepcopy
+
 #dir is 0 for right and 1 for down
 #(0,0) is the top left corner of the board
 def initLetterScores():
     letterScores = {"-":0,"aeioulnrst":1,"dg":2,"bcmp":3,"fhvwy":4,"k":1,"jx":8,"qz":10}
     return letterScores
 
-def initBoard():
+def initBoard(bonus):
     board = []
-    initDict = {"letter":"0","letterBonus":1,"wordBonus":1}
-    list = []
-    for j in range(15):
-        list.append(initDict)
+    initList = ["0",1,1]#letter,letterbonus,wordbonus
+    lst = []
+    print deepcopy(initList)
+    a = deepcopy(initList)
     for i in range(15):
-        board.append(list)
+        lst.append(deepcopy(initList))
+    for i in range(15):
+        board.append(deepcopy(lst))
+    
+    '''
+    list = [initList[:]*15]
+    board = [list[:]]*15
+    '''
+    for j in range(4):
+        for i in bonus[j]:
+            type = j//2 + 1
+            val = j % 2 + 2
+            print i,j,type,val
+            board[i[0]][i[1]][type],board[14 - i[0]][i[1]][type],board[i[0]][14 - i[1]][type],board[14 - i[0]][14 - i[1]][type] = [val]*4
+            
+    print board[0]
     return board
     #*15 doesn't work
+
+def initBonus():
+    #bonus = [dls,dws,tls,tws]
+    dls = [[0,3],[2,6],[3,0],[3,7],[6,2],[6,6],[7,3]]
+    dws = [[1,1],[2,2],[3,3],[4,4]]
+    tls = [[1,5],[5,1],[5,5]]
+    tws = [[0,0],[0,7],[7,0]]
+    b = [dls,dws,tls,tws]
+    bonus = [dls[:],dws[:],tls[:],tws[:]]
+    '''
+    for j in range(4):
+        for i in b[j]:
+            print b[j]
+            bonus[j].append([i[0], 14 - i[1]])
+            bonus[j].append([14 - i[0], i[1]])
+            bonus[j].append([14 - i[0], 14 - i[1]])
+            '''
+ 
+    return bonus
 
 def computeScore(word, row, col, dir,board,letterScores):#board, row no, col no, dir
     sum = 0
@@ -30,6 +66,6 @@ def computeScore(word, row, col, dir,board,letterScores):#board, row no, col no,
     return sum
 
 
-board = initBoard()
+board = initBoard(initBonus())
 letterScores = initLetterScores()
-print(computeScore("mentally",1,6,0,board,letterScores))
+#print(computeScore("mentally",1,6,0,board,letterScores))
